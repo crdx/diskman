800fbe5cc0a4108ac54fa2b536ae2fddcc846fd4
crdx <pub@crdx.org>
2025-10-19 20:22:09 +0100

    Remove require_all gem

--- lib/diskman.rb
+++ lib/diskman.rb
@@ -1,4 +1,3 @@
-require 'require_all'
 require 'colorize'
 
 require 'ostruct'
@@ -9,4 +8,17 @@ module Diskman
     end
 end
 
-require_rel 'diskman'
+require_relative 'diskman/chooser'
+require_relative 'diskman/commands/choose_device'
+require_relative 'diskman/commands/choose_partition'
+require_relative 'diskman/commands/clone'
+require_relative 'diskman/commands/fdisk'
+require_relative 'diskman/commands/ls'
+require_relative 'diskman/commands/mkfs'
+require_relative 'diskman/commands/write'
+require_relative 'diskman/confirmer'
+require_relative 'diskman/device'
+require_relative 'diskman/mount'
+require_relative 'diskman/root_device'
+require_relative 'diskman/system'
+require_relative 'diskman/version'
